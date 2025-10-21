cb92b1253235def9f766b95ef3e3ca502d702e81
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2025-10-21 10:03:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,12 +40,30 @@ function displayBooks(data){
     data.forEach(book => {
         tbody.append(`<tr>
             <td>${book['م']}</td>
-            <td>${book['اسم الكتاب']}</td>
+            <td class="book-title" style="cursor:pointer; color:blue;">${book['اسم الكتاب']}</td>
             <td>${book['اسم المؤلف']}</td>
             <td>${book['اسم المحقق']}</td>
             <td>${book['دار النشر']}</td>
             <td>${book['سنة النشر']}</td>
             <td>${book['سعر البيع']}</td>
         </tr>`);
+    });
+
+    // تفعيل النقر على اسم الكتاب لفتح Modal
+    $('.book-title').click(function(){
+        const title = $(this).text();
+        const book = data.find(b => b['اسم الكتاب'] === title);
+        if(book){
+            $('#bookModalLabel').text(book['اسم الكتاب']);
+            $('#modalBody').html(`
+                <p><strong>المؤلف:</strong> ${book['اسم المؤلف']}</p>
+                <p><strong>المحقق:</strong> ${book['اسم المحقق']}</p>
+                <p><strong>دار النشر:</strong> ${book['دار النشر']}</p>
+                <p><strong>سنة النشر:</strong> ${book['سنة النشر']}</p>
+                <p><strong>سعر البيع:</strong> ${book['سعر البيع']}</p>
+            `);
+            var myModal = new bootstrap.Modal(document.getElementById('bookModal'));
+            myModal.show();
+        }
     });
 }
